69a752245c887c59c591a639a83128b0f12be5c5
istiaq-tanim <istiaq.tanim17@gmail.com>
2024-09-08 23:44:36 +0600

    deploy

--- index.js
+++ index.js
@@ -5,38 +5,116 @@ const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection URL
+
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
-        // Connect to MongoDB
-        await client.connect();
+
+        // await client.connect();
         console.log("Connected to MongoDB");
 
         const db = client.db('book-store');
         const bookCollection = db.collection('books');
+        const userCollection = db.collection("users")
+
+
+        app.post('/register', async (req, res) => {
+            const { name, email, password } = req.body;
+
+
+            const existingUser = await userCollection.findOne({ email });
+            if (existingUser) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'User already exists'
+                });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            await userCollection.insertOne({ name, email, password: hashedPassword, role: "USER" });
+
+            res.status(201).json({
+                success: true,
+                message: 'User registered successfully'
+            });
+        });
+
+        // User Login
+        app.post('/login', async (req, res) => {
+            const { email, password } = req.body;
+
+            // Find user by email
+            const user = await userCollection.findOne({ email });
+            if (!user) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Compare hashed password
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRES_IN });
+
+            res.json({
+                success: true,
+                message: 'Login successful',
+                token
+            });
+        });
 
         app.get("/books", async (req, res) => {
-            const books = await bookCollection.find().toArray()
-            return res.status(200).json({
+            const query = req.query
+            let filter = { $and: [] }
+
+            if (query?.ratings) {
+                const ratings = query?.ratings.split("|").map(item => parseInt(item, 10))
+                filter.$and.push({ ratings: { $in: ratings } })
+            };
+
+            if (query?.category) {
+                const categories = query.category.split("|").map(item => ({
+                    category: { $regex: new RegExp(item, "i") }
+                }))
+                filter.$and.push({ $or: categories });
+            }
+
+            if (query?.price) {
+                const priceRanges = query.price.split("|").map(range => {
+                    const [min, max] = range.split("-").map(Number);
+                    return { price: { $gte: min, $lte: max } };
+                });
+                filter.$and.push({ $or: priceRanges });
+            }
+
+            if (filter.$and.length === 0) {
+                delete filter.$and;
+            }
+            const books = await bookCollection.find(filter).toArray()
+
+            res.status(201).json({
                 success: true,
-                message: "Books Retrieved Successfully",
+                message: 'Books is Fetching',
                 data: books
-            })
+            });
+
         })
 
         app.get("/books/:id", async (req, res) => {
             const { id } = req.params
-            const query = { _id: new ObjectId(id) }
+            console.log(id)
+            const query = { _id: new ObjectId(id) };
             const book = await bookCollection.findOne(query)
             return res.status(200).json({
                 success: true,
