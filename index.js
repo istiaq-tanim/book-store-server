69e397c0f2898a4ddffd9e2a246dcd14d5a70529
istiaq-tanim <istiaq.tanim17@gmail.com>
2024-09-28 10:03:46 +0600

    admin-stat

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt');
 const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const SSLCommerzPayment = require('sslcommerz-lts')
+
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASSWORD
+const is_live = false
 
 
 const app = express();
@@ -185,6 +190,87 @@ async function run() {
                 result
             });
         })
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const trans_id = new ObjectId().toString()
+            const data = {
+                total_amount: order.total,
+                currency: order.currency,
+                tran_id: trans_id,
+                success_url: `${process.env.BASE_URL}/payment-success/${trans_id}`,
+                fail_url: 'http://localhost:3030/fail',
+                cancel_url: 'http://localhost:3030/cancel',
+                ipn_url: 'http://localhost:3030/ipn',
+                shipping_method: 'Courier',
+                product_name: 'Computer.',
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: order.name,
+                cus_email: order.email,
+                cus_add1: order.address,
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: order.contact,
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+            };
+
+            const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+            sslcz.init(data).then(apiResponse => {
+                let GatewayPageURL = apiResponse.GatewayPageURL
+                res.send({ url: GatewayPageURL })
+                const finalOrder = {
+                    email: order?.email,
+                    paidStatus: false,
+                    deliveryStatus: false,
+                    transactionId: trans_id,
+                    totalPrice: order?.total,
+                    address: order?.address,
+                    orderDate: new Date(),
+                    products: order?.orderProducts.map(({ author, title, image, quantity, originalPrice }) => {
+                        return {
+                            author,
+                            title,
+                            image,
+                            quantity,
+                            price: originalPrice
+                        }
+                    })
+
+                }
+                const result = paymentCollection.insertOne(finalOrder)
+            });
+            app.post("/payment-success/:tranId", async (req, res) => {
+                const { tranId } = req.params
+                console.log(tranId)
+                const result = await paymentCollection.updateOne({ transactionId: tranId }, {
+                    $set: {
+                        paidStatus: true
+                    }
+                })
+                if (result?.modifiedCount > 0) {
+                    res.redirect(`${process.env.FBASE_URL}/payment/success/${tranId}`)
+                }
+            })
+        })
+
+        app.get("/orders", async (req, res) => {
+            const orderBookList = await paymentCollection.find().toArray()
+            return res.status(200).json({
+                success: true,
+                message: "Payment Retrieved Successfully",
+                data: orderBookList
+            })
+        })
 
         app.get("/payment", async (req, res) => {
             const { email } = req.query
@@ -194,6 +280,71 @@ async function run() {
                 message: "Payment Retrieved Successfully",
                 data: orderBookList
             })
+        })
+
+        app.get("/statistic", async (req, res) => {
+            const { email } = req.query
+            const userStat = await paymentCollection.aggregate([
+                { $match: { email: email } },
+                {
+                    $group: {
+                        _id: "$email",
+                        totalPayment: { $sum: { $cond: [{ $eq: ["$paidStatus", true] }, "$totalPrice", 0] } },
+                        pendingOrders: { $sum: { $cond: [{ $eq: ["$deliveryStatus", false] }, 1, 0] } },
+                        deliveryOrders: { $sum: { $cond: [{ $eq: ["$deliveryStatus", true] }, 1, 0] } }
+                    }
+                }
+
+            ]).toArray()
+            return res.status(200).json({
+                success: true,
+                message: "Payment Retrieved Successfully",
+                data: userStat
+            })
+        })
+
+        app.get("/admin-stat", async (req, res) => {
+            const stats = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalPayment: {
+                            $sum: "$totalPrice"
+                        },
+                        pendingOrders: {
+                            $sum: { $cond: [{ $eq: ["$deliveryStatus", false] }, 1, 0] }
+                        },
+                        deliveryOrders: {
+                            $sum: { $cond: [{ $eq: ["$deliveryStatus", true] }, 1, 0] }
+                        }
+                    }
+                }
+            ]).toArray();
+            return res.status(200).json({
+                success: true,
+                message: "Admin Statistic Retrieved Successfully",
+                data: stats
+            })
+        })
+
+        app.patch("/orders/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const order = await paymentCollection.findOne(query)
+            console.log(order)
+
+            if (!order) {
+                return res.status(404).send({ message: "Order not found" });
+            }
+            const updateDoc = {
+                $set: {
+                    deliveryStatus: !order.deliveryStatus
+                }
+            };
+
+            const result = await paymentCollection.updateOne(query, updateDoc)
+            console.log(result)
+            res.send(result)
         })
 
         app.listen(port, () => {
